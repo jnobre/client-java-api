37d4401dc59993e7c47011cf341d332e5995f9fc
jnobre <joaoanobre@gmail.com>
2017-11-14 11:01:48 +0000

    Update junit test

--- src/testing/GetVersions.java
+++ src/testing/GetVersions.java
@@ -22,7 +22,7 @@ public class GetVersions {
 		
 		String versionsSTR = TextSearchConn.getVersions( Constants.queryTestSimple , 0 , numberVersions );
 		TextSearchResponse versions = (TextSearchResponse) JSONParsing.parsed( versionsSTR, JSONClasses.Response );
-		//test if you can find at least the first 5 routes
+		//test if you can find at least the first 5 of preserved web pages
 		assertEquals( numberVersions, versions.getItens( ).size( ) );
 	}
 	
